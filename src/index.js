889a7c95a3c64402f6e18b365742c0829482553d
imilovanov <milovanov.ivan.94@gmail.com>
2020-08-02 13:24:58 +0700

    Add css grid

--- src/index.js
+++ src/index.js
@@ -1,16 +1,88 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const electronInstaller = require('electron-winstaller');
+
+var appRoot = require('app-root-path');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
 }
 
+// this should be placed at top of main.js to handle setup events quickly
+if (handleSquirrelEvent()) {
+  // squirrel event handled and app will exit in 1000ms, so don't do anything else
+  return;
+}
+
+function handleSquirrelEvent() {
+  if (process.argv.length === 1) {
+    return false;
+  }
+
+  const ChildProcess = require('child_process');
+  const path = require('path');
+
+  const appFolder = path.resolve(process.execPath, '..');
+  const rootAtomFolder = path.resolve(appFolder, '..');
+  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+  const exeName = path.basename(process.execPath);
+
+  const spawn = function(command, args) {
+    let spawnedProcess, error;
+
+    try {
+      spawnedProcess = ChildProcess.spawn(command, args, {detached: true});
+    } catch (error) {}
+
+    return spawnedProcess;
+  };
+
+  const spawnUpdate = function(args) {
+    return spawn(updateDotExe, args);
+  };
+
+  const squirrelEvent = process.argv[1];
+  switch (squirrelEvent) {
+    case '--squirrel-install':
+    case '--squirrel-updated':
+      // Optionally do things such as:
+      // - Add your .exe to the PATH
+      // - Write to the registry for things like file associations and
+      //   explorer context menus
+
+      // Install desktop and start menu shortcuts
+      spawnUpdate(['--createShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-uninstall':
+      // Undo anything you did in the --squirrel-install and
+      // --squirrel-updated handlers
+
+      // Remove desktop and start menu shortcuts
+      spawnUpdate(['--removeShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-obsolete':
+      // This is called on the outgoing version of your app before
+      // we update to the new version - it's the opposite of
+      // --squirrel-updated
+
+      app.quit();
+      return true;
+  }
+};
+
 const createWindow = () => {
   // Create the browser window.
+  createInstaller();
   const mainWindow = new BrowserWindow({
     width: 1200,
-    height: 1000,
+    height: 1000
   });
 
   // and load the index.html of the app.
@@ -45,3 +117,20 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+
+
+async function createInstaller() {
+  console.log('test');
+  console.log((appRoot.path + '\\tmp\\'));
+try {
+  await electronInstaller.createWindowsInstaller({
+    appDirectory: appRoot.path,
+    outputDirectory: appRoot.path + '\\tmp',
+    authors: 'My App Inc.',
+    exe: 'myapp.exe'
+  });
+  console.log('It worked!');
+} catch (e) {
+  console.log(`No dice: ${e.message}`);
+}
+}
